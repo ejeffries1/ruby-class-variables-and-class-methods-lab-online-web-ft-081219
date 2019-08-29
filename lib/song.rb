8cdbeaac71384159fe57051a7119ce7578c7fe0a
Evan Jeffries <ejeffries14@gmail.com>
2019-08-29 01:38:07 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+class Song
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+  @@genres.uniq
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genre_count
+    gen = {}
+    @@genres.each do |genre|
+      gen[genre] = @@genres.count
+    end
+    gen
+  end
+  
+  def self.artist_count
+     art = {}
+    @@artists.collect do |artist|
+      art[artist] = @@artists.count
+    end
+    art
+  end
+  
+end
+
+
+
+
